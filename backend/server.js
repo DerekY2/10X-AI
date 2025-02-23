ba69bb8ffe3aa046b15894f3f7b7d8fea8ac2fd3
DerekY2 <derek.yu869@gmail.com>
2025-02-22 19:58:27 -0500

    removed redundant functions

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,6 @@ require('dotenv').config()
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const fs = require("fs");
-const Papa = require("papaparse");
 const openai = require("./routes/openai")
 const deepseek = require("./routes/deepseek")
 const openrouter = require("./routes/openRouter")
@@ -18,30 +16,6 @@ app.use(bodyParser.json());
 app.use("/openai", openai)
 app.use("/deepseek", deepseek)
 app.use("/openrouter", openrouter)
-
-// Load spreadsheet data
-let faqData = [];
-
-function parseCSV(filePath) {
-    const file = fs.readFileSync(filePath, "utf8");
-    Papa.parse(file, {
-        header: true,
-        skipEmptyLines: true,
-        complete: (result) => {
-            console.log("CSV Data Loaded:", result.data);
-        },
-    });
-}
-
-function parseJSON(data){
-  Papa.parse(data, {header:true, complete: (result)=>{
-    console.log("JSON Data loaded:",result.data)
-    // return results;
-  }})
-}
-
-parseCSV("users.csv");
-console.log(fs.readFileSync("users.json", "utf8"))
 
 app.get('/', (req,res)=>{
   res.send('Hi')
