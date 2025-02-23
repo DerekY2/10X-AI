6fe4ffda33fbb09edf48a11848e26aae103fdaec
DerekY2 <derek.yu869@gmail.com>
2025-02-23 17:37:43 -0500

    docker

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const deepseek = require("./routes/deepseek")
 const openrouter = require("./routes/openRouter")
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
