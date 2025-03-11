20e5f9a8359d23a07e579c7e09c9a468dd0b5267
DerekY2 <derek.yu869@gmail.com>
2025-03-11 15:07:25 -0400

    readme update

--- frontend/index.js
+++ frontend/index.js
@@ -1,4 +1,6 @@
-const DEEPSEEK_V3 = "http://localhost:5000/deepseek/v3";
+// const DEEPSEEK_V3 = "http://localhost:5000/deepseek/v3";
+// const OLLAMA_R1 = "http://localhost:5000/ollama/deepseek/r1/7b";
+const OPEN_ROUTER = "http://localhost:5000/openrouter/deepseek/v3";
 const SERVER = OPEN_ROUTER;
 const messages = [];
 
